6b240e491acc7756d74187eca4a093ae2b7c1cc3
jujube0 <kians2@naver.com>
2024-05-29 18:38:13 +0900

    [setting] xcode project 추가

---
+++ SearchBook/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SearchBook
+//
+//  Created by 김가영 on 5/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+}

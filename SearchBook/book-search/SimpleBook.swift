15580d348a15cd8a576badd556abe73e63b69693
jujube0 <60654009+jujube0@users.noreply.github.com>
2024-06-02 02:19:51 +0900

    [refactor] reloadData 시 모든 cell이 reload됨 (#18)

--- SearchBook/book-search/SimpleBook.swift
+++ SearchBook/book-search/SimpleBook.swift
@@ -7,13 +7,18 @@
 
 import Foundation
 
-class SimpleBook: Decodable {
+class SimpleBook: Identifiable, Decodable {
+    let id: UUID = UUID()
     let title: String?
     let subtitle: String?
     let isbn13: String?
     let price: String?
     let image: String?
     let url: String?
+    
+    private enum CodingKeys: String, CodingKey {
+        case title, subtitle, isbn13, price, image, url
+    }
     
     init(title: String?, subtitle: String?, isbn13: String?, price: String?, image: String?, url: String?) {
         self.title = title
